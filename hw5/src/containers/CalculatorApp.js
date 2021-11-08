7f08fd1d17f8c5e4193f2c39e105e60789d134e7
peifish1124 <b08705026@ntu.edu.tw>
2021-11-08 19:47:29 +0800

    [Chen] hw5- final(1)

--- hw5/src/containers/CalculatorApp.js
+++ hw5/src/containers/CalculatorApp.js
@@ -1,6 +1,5 @@
 import CalButton from "../components/button";
-import cloneDeep from 'lodash/cloneDeep';
-import { useState,useEffect } from 'react'
+import { useState } from 'react'
 
 const CalculatorApp = () => {
 
@@ -8,7 +7,9 @@ const CalculatorApp = () => {
   const [store, setStore] = useState(0);
   const [reset, setReset] = useState(false);
   const [operator, setOperator] = useState('');
-
+  const [index, setIndex] = useState(1);
+  const [point, setPoint] = useState('');
+  const [memory, setMemory] = useState(0);
   const ac = () => {
     setCount(0);
     setStore(0);
@@ -17,7 +18,11 @@ const CalculatorApp = () => {
   }
   const addNumber = (value) => {
     if(reset === false){
-      setCount(count*10+value);
+      if(point === '.') {
+        setCount(count+value*Math.pow(0.1,index));
+        setIndex(index+1);
+      }
+      else setCount(count*10+value);
     } else {
       setCount(value);
       setReset(false);
@@ -43,14 +48,19 @@ const CalculatorApp = () => {
 
       if(value === '+') {
         setOperator('+');
+        setIndex(1); setPoint('');
       } else if (value === '-') {
         setOperator('-');
+        setIndex(1); setPoint('');
       } else if (value === 'x') {
         setOperator('x');
+        setIndex(1); setPoint('');
       } else if (value === '/') {
         setOperator('/');
+        setIndex(1); setPoint('');
       } else if (value === '%') {
         setOperator('%');
+        setIndex(1); setPoint('');
       }
   }
   const equalNum = () => {
@@ -79,12 +89,23 @@ const CalculatorApp = () => {
       setStore(0);
       setReset(true);
       setOperator('');
-    }     
+    }  
+  }
+
+  const memoryNum = (value) => { 
+    if(value==='+')setMemory(memory+count);
+    else setMemory(memory-count);
+  }
+  const mc = () => {
+    setMemory(0);
   }
 
   const backSpace = () => {
-    var last = count % 10;
-    setCount((count-last)/10);
+    // var last = count % 10;
+    // setCount((count-last)/10);
+    var str = String(count);
+    var cutStr = str.substring(0, str.length - 1);
+    setCount(parseFloat(cutStr));
   }
 
   const opposite = () => {
@@ -100,6 +121,7 @@ const CalculatorApp = () => {
   }
   const smallerNum = () => {
     setCount(count/100);
+    setReset(true);
   }
 
   return (
@@ -122,7 +144,7 @@ const CalculatorApp = () => {
         </div>
         <div className="line_2">
           <CalButton text='√' index='8' onClick={ () => powerHalfNum() }/>  
-          <CalButton text='log' index='9'/> 
+          <CalButton text='%' index='9' onClick={ () => smallerNum() }/> 
           <CalButton text='x^2' index='10' onClick={ () => powerNum() }/> 
           <CalButton text='4' index='11' onClick={ () => addNumber(4) }/> 
           <CalButton text='5' index='12' onClick={ () => addNumber(5) }/>
@@ -130,20 +152,22 @@ const CalculatorApp = () => {
           <CalButton text='-' index='14' onClick={ () => operateNum('-') }/>
         </div>
         <div className="line_3">
-          <CalButton text='e' index='15'/>  
-          <CalButton text='%' index='16' onClick={ () => smallerNum() }/> 
-          <CalButton text='◀' index='17' onClick={ () => backSpace() }/> 
+          <CalButton text='MR+' index='15' onClick={ () => memoryNum('+')}/>  
+          <CalButton text='MC' index='16' onClick={ () => mc() }/> 
+          <CalButton text='MR-' index='17' onClick={ () => memoryNum('-')}/>
           <CalButton text='1' index='18' onClick={ () => addNumber(1) }/> 
           <CalButton text='2' index='19' onClick={ () => addNumber(2) }/>
           <CalButton text='3' index='20' onClick={ () => addNumber(3) }/>
           <CalButton text='+' index='21' onClick={ () => operateNum('+') }/>
         </div>
         <div className="line_4">
-          <CalButton text='MR' index='22'/>  
+          {/* <CalButton text='MR' index='22'/>  
           <CalButton text='MC' index='23'/> 
-          <CalButton text='MC' index='24'/>
+          <CalButton text='MC' index='24'/> */}
+          <div className="left">M: {memory}</div>
+          <CalButton text='◀' index='24' onClick={ () => backSpace() }/> 
           <CalButton text='0' index='25' onClick={ () => addNumber(0) }/>
-          <CalButton text='.' index='26' /> 
+          <CalButton text='.' index='26' onClick={ () => setPoint('.') }/> 
           <CalButton text='/' index='27' onClick={ () => operateNum('/') }/> 
           <CalButton text='=' index='28' onClick={ () => equalNum() }/>
         </div>
